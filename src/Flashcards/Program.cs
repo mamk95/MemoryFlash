5b6e4dc3ea1934f9c3ae2cc823dfbd044f23fab9
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2022-09-24 22:39:57 +0200

    Remove template code

--- src/Flashcards/Program.cs
+++ src/Flashcards/Program.cs
@@ -1,4 +1,3 @@
-using Flashcards.Data;
 using Flashcards.Features.Flashcard.Repository;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<FlashcardRepository>();
 
 var app = builder.Build();
